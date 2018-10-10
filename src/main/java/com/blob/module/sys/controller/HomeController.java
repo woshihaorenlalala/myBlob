7e581a82e910b1e8e29d2cb20d7ae8078dca6446
wxc <531742655@qq.com>
2018-10-10 15:41:54 +0800

    增加log4j2

--- src/main/java/com/blob/module/sys/controller/HomeController.java
+++ src/main/java/com/blob/module/sys/controller/HomeController.java
@@ -1,15 +1,12 @@
 package com.blob.module.sys.controller;
 
-import com.blob.module.common.sys.entity.Constant;
-import com.blob.module.common.sys.entity.HttpResult;
-import org.apache.log4j.Logger;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,8 +18,11 @@ import java.util.Map;
 @Controller
 public class HomeController {
 
+    private final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     @RequestMapping(value = "/index")
-    public String index(){
+    public String index() {
+        logger.info("------------------------进入首页-----------------");
         ModelAndView mv = new ModelAndView("index");
         return "/index";
     }
@@ -33,19 +33,19 @@ public class HomeController {
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login(HttpServletRequest request, Map<String,Object> map){
-
+    public String login(HttpServletRequest request, Map<String, Object> map) {
+        logger.info("------------------------登陆-----------------");
         String exception = (String) request.getAttribute("shiroLoginFailure");
 
         String msg = "";
-        if(null != exception) {
-            if(UnknownAccountException.class.getName().equals(exception)) {
+        if (null != exception) {
+            if (UnknownAccountException.class.getName().equals(exception)) {
                 msg = "账号不存在";
-            }else if(IncorrectCredentialsException.class.getName().equals(exception)) {
+            } else if (IncorrectCredentialsException.class.getName().equals(exception)) {
                 msg = "密码不正确";
-            }else if("kaptchaValidateFailed".equals(exception)){
+            } else if ("kaptchaValidateFailed".equals(exception)) {
                 msg = "验证码错误";
-            }else {
+            } else {
                 msg = "else:" + exception;
             }
         }
